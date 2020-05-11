a3ceaa77871a06a86c865a0ec8d673bc3be7252d
azztt <ee3180878@iitd.ac.in>
2020-05-11 23:09:22 +0530

    added build to .gitignore

--- build/src/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-const server = app_1.default.listen(app_1.default.get('port'), () => {
-    console.log(`App is Running at http://localhost:${app_1.default.get('port')}`);
-    console.log('  Press CTRL-C to stop\n');
-});
-exports.default = server;
-//# sourceMappingURL=server.js.map
